dbb4660dcbfbd0ea0f1bb0f7953239115b2d0498
adrift <1219660880@qq.com>
2021-04-01 10:11:40 +0800

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,21 @@
-ln -s ~/configs/.vimrc ~/.vimrc
 ln -s ~/configs/.tmux.conf ~/.tmux.conf
 ln -s ~/configs/.tmux ~/.tmux
+
+# init tpm plugin
+git submodule init
+git submodule update
+
+#
+source ~/.tmux.conf
+# install tmux plugins
+~/.tmux/plugins/tpm/bin/install_plugins
+
+# vim configs
+ln -s ~/configs/.vimrc ~/.vimrc
+
+# download vim-plug
+curl -fLo ~/configs/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -s ~/confing/.vim ~/.vim
+
+
 
