e2a8423de2cf8b9654db1048f8a61ad1bd95e59a
adrift <1219660880@qq.com>
2021-04-11 10:07:01 +0800

    modify setup.sh and remove submodule cache

---
+++ .tmux/.tmux
@@ -0,0 +1,1 @@
+/home/adrift/configs/.tmux
--- setup.sh
+++ setup.sh
@@ -1,21 +1,33 @@
+if [ -f ~/.tmux.conf ]; then
+    mv ~/.tmux.conf ~/.tmux.conf.bak
+fi
 ln -s ~/configs/.tmux.conf ~/.tmux.conf
+
+if [ -d ~/.tmux ]; then
+    mv ~/.tmux ~/.tmux.bak
+fi
 ln -s ~/configs/.tmux ~/.tmux
 
 # init tpm plugin
-git submodule init
-git submodule update
+git submodule update --init --recursive
 
 #
-source ~/.tmux.conf
+tmux source ~/.tmux.conf
 # install tmux plugins
 # Press prefix + I (capital i, as in Install) to fetch the plugin.
 
 # vim configs
+if [ -f ~/.vimrc ]; then
+    mv ~/.vimrc ~/.vimrc.bak
+fi
 ln -s ~/configs/.vimrc ~/.vimrc
 
 # download vim-plug
 curl -fLo ~/configs/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s ~/confing/.vim ~/.vim
+if [ -d ~/.vim ]; then
+    mv ~/.vim ~/.vim.bak
+fi
+ln -s ~/configs/.vim ~/.vim
 # use :PlugInstall to install vim plugins
 
 
